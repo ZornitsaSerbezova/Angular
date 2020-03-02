ade393307853cb8a563843d04d3ee66a0f9e6d22
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2020-03-02 09:47:42 +0200

    git push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import "@balkangraph/orgchart.js";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orgchart';
+
+  constructor() { }
+
+  ngOnInit() {
+    var chart = new OrgChart(document.getElementById('tree'), {
+      template: "olivia",
+
+            nodeBinding: {
+                field_0: "name",
+                field_1: "title",
+                img_0: "img"
+            },
+            tags: {
+                HRs: {
+                    group: true,
+                    groupName: "HR Team",
+                    groupState: OrgChart.EXPAND,
+
+                    template: "group_grey"
+                }
+            },
+
+            nodes: [
+                { id: 1, name: "Billy Moore", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
+                { id: 2, pid: 1, name: "Billie Rose", title: "Dev Team Lead", img: "https://cdn.balkan.app/shared/5.jpg" },
+
+                { id: 3, pid: 1, tags: ["HRs"], name: "Glenn Bell", title: "HR", img: "https://cdn.balkan.app/shared/10.jpg" },
+                { id: 4, pid: 1, tags: ["HRs"], name: "Blair Francis", title: "HR", img: "https://cdn.balkan.app/shared/11.jpg" }
+            ]
+        
+   })
+
+  }
 }
